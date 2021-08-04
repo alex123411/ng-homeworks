c1fddfe0eed0e5060e9258e1932a2ee84e3a607d
alex123411 <57320718+alex123411@users.noreply.github.com>
2021-08-04 23:56:34 +0300

    task1 done

--- task1/index.ts
+++ task1/index.ts
@@ -1,1 +1,27 @@
 // Build a Mortgage Claculator using Rxjs and calculateMortgage method
+import { fromEvent } from 'rxjs';
+import { calculateMortgage } from './calculate';
+
+const loanAmountInput = <HTMLInputElement>document.getElementById('loanAmount');
+const loanInterestInput = <HTMLInputElement>(
+  document.getElementById('loanInterest')
+);
+const loanLengthSelect = <HTMLInputElement>(
+  document.getElementById('loanLength')
+);
+
+const calcMortageEvent = fromEvent(
+  [loanAmountInput, loanInterestInput, loanLengthSelect],
+  'change'
+).subscribe(x => {
+  let res = calculateMortgage(
+    loanInterestInput.value,
+    loanAmountInput.value,
+    loanLengthSelect.value
+  );
+  if (res != 'NaN') {
+    document.getElementById('result').innerHTML = res;
+  } else {
+    document.getElementById('result').innerHTML = '';
+  }
+});
